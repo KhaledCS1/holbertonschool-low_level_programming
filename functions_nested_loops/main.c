62602be5398f9b746af19f020cd2d84b51ff00c0
KhaledCS1 <10688@holbertonstudents.com>
2025-02-20 20:45:32 +0000

    Task 3

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+    print_alphabet_x10();  /* Call function to print alphabet 10 times */
+    return (0);
+}
