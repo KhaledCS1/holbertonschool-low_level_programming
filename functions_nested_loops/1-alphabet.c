a2d48520cb9332c71be104f08d327928bd754f47
KhaledCS1 <10688@holbertonstudents.com>
2025-02-20 20:22:45 +0000

    Task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a new line
- */
+* print_alphabet - Prints the lowercase alphabet followed by a new line
+*/
 void print_alphabet(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');  /* Second _putchar used to add a new line */
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');  /* Second _putchar used to add a new line */
 }
