60242dad60fe7ac535ac8c46e3123dae5a714710
KhaledCS1 <10688@holbertonstudents.com>
2025-02-20 21:28:47 +0000

    Task 6

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - Determines if the number is positive, zero, or negative.
+* @n: The number to check.
+*
+* Return: 1 if n is positive, 0 if n is zero, -1 if n is negative.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
