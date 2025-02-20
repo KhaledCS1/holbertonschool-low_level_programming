1537084fe34c38b3c3161f6534248a19ba45622e
KhaledCS1 <10688@holbertonstudents.com>
2025-02-20 21:02:46 +0000

    Task 4

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to standard output.
- * @c: The character to print.
- *
- * Return: 1 on success, -1 on error.
- */
+* _putchar - Writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
-    return (write(1, &c, 1));  /* Writes a single character to stdout */
+return (write(1, &c, 1));
 }
