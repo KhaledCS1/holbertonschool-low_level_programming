4ff37c977a147b1358ae7efe73338ddca4a9e09b
KhaledCS1 <10688@holbertonstudents.com>
2025-02-25 12:03:14 +0000

    Task 4

---
+++ more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Calls the print_most_numbers function to test output.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
