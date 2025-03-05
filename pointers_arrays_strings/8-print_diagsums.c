71464c4a208302a137a4430c05872e58267558d8
KhaledCS1 <10688@holbertonstudents.com>
2025-03-05 16:32:53 +0000

    Added Tasks

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_diagsums - print the sum the diognals of a square matrix of integer
- *
- * @a: This is the array to sum
- * @size: This is the length of the array
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: square matrix of which we print the symof diagonals
+ * @size: size of the matrix
  */
-
 void print_diagsums(int *a, int size)
 {
-	int ascedent, descendet, i, sumAscedent, sumDescedent;
+	int i;
 
-	sumAscedent = sumDescedent = 0;
-	ascedent = 0;
-	descendet = size - 1;
+	unsigned int sum, sum1;
+
+	 sum = 0;
+	 sum1 = 0;
+
 	for (i = 0; i < size; i++)
 	{
-		sumAscedent += *(a + i * size + ascedent);
-		sumDescedent += *(a + i * size + descendet);
-		ascedent += 1;
-		descendet -= 1;
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
-	printf("%d, %d\n", sumAscedent, sumDescedent);
+
+	printf("%d, %d\n", sum, sum1);
 }
