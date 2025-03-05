128963f50729ccccdfb828d9ba93c3c7613b37e2
KhaledCS1 <10688@holbertonstudents.com>
2025-03-05 16:10:11 +0000

    Added Tasks

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -2,16 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - Check the code.
+ * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
-    int len;
+    char *s = "hello";
+    char *f;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
     return (0);
 }
